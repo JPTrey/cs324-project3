c3d4b1b13d2cdae98302e209b8618e874fd3fd26
Jon Paul <jonpaul.simonelli@gmail.com>
2014-04-08 11:48:14 -0400

    initial commit

---
+++ src/classes/Main.java
@@ -0,0 +1,15 @@
+package classes;
+
+/**
+ * 
+ * @author jsimonel
+ * Source hosted at: https://github.com/JPTrey/cs324-project3
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
