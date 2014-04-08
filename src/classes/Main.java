510528d9387e15cd1a17f7a16711774b1bebe869
Jon Paul <jonpaul.simonelli@gmail.com>
2014-04-08 12:08:59 -0400

    adding coding style

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -8,11 +8,11 @@ package classes;
  * Declare all variables used within the scope of a class/method at the top.
  * Follow indentation patterns.
  * All methods names must be verbs; if it cannot be described, it is performing too many operations.
- * Methods do NOT return any parameters. Parameters should be final whenever possible.
+ * Methods should NOT return any input parameters. Parameters should be final whenever possible.
  * Use setters and getters. Variables/methods should be private/final whenever possible.
  * Use the Output class for console statements. Output.debug calls are ignored whenever Main.debug == false.
  * Pass the least amount of data possible between methods/classes. Avoid passing objects/arrays.
- * Implement threads using Runnable. Do NOT extend Thread.
+ * Implement Runnable. Do NOT extend Thread.
  */
 public class Main {
 
