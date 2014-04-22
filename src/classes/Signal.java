2643cd210a302ed185f42d55f6f97ebe70651f6e
mlekena <mlekena@skidmore.edu>
2014-04-22 12:05:44 -0400

    complete event signal Theko

--- src/classes/Signal.java
+++ src/classes/Signal.java
@@ -1,6 +1,56 @@
 package classes;
 
 public class Signal {
+	
+	public enum EventState{TICK,TOCK};
+	
+	protected EventState value;
+	
+	public Signal( EventState val)
+	{
+		value = val;
+	}
+	
+	public Signal()
+	{
+		value = EventState.TOCK;
+	}
+	
+	public void await(EventState ev)
+	{
+		while (value != ev){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	public void set()
+	{
+		value = EventState.TICK;
+		notifyAll();
+	}
+	
+	public void reset()
+	{
+		value = EventState.TOCK;
+		notifyAll();
+	}
 
-
+	public void toggle()
+	{
+		if (value == EventState.TICK)
+		{
+			value = EventState.TOCK;
+			notifyAll();
+		}
+		else
+		{
+			value = EventState.TICK;
+			notifyAll();
+		}
+	}
 }
