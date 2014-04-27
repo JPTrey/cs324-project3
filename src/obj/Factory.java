c9578d7a091294e8137e87a33ca7003ef20b5ea5
mlekena <mlekena@skidmore.edu>
2014-04-27 14:55:14 -0400

    Built factory using int as inventory. Theko

--- src/obj/Factory.java
+++ src/obj/Factory.java
@@ -1,18 +1,38 @@
 package obj;
 
 import classes.Signal;
+import classes.Signal.EventState;
 
 public class Factory implements Runnable {
-	private Signal		factoryStore;		// reference to signal between this factory and Store
 	
+	private Signal		factoryStore;		// reference to signal between this factory and Store
+	private int inventory;
+	private int inventoryCap;
 	public Factory(int id, Signal factoryStore) {
 		this.factoryStore = factoryStore;
+		inventory = 0;
+		inventoryCap = 200;
+	}
+	
+	public Factory(int id, Signal factoryStore, int startingInventory, int startingCap) {
+		this.factoryStore = factoryStore;
+		inventory = startingInventory;
+		inventoryCap = startingCap;
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-
+		while (true)
+		{
+			factoryStore.await(EventState.TICK);
+			if (inventory == inventoryCap || inventory+20 >= inventoryCap)
+			{
+				continue;
+			}
+			inventory += 20;
+			
+		}
 	}
 
 }
